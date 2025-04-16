ee0bed6980472b99ac305bf7d80d4a8bdfd90a5e
SergeyZimakov <zimakovs93@gmail.com>
2025-04-16 16:10:25 +0300

    refactor dates

--- Core/Interface/Repository/IShiftRepository.cs
+++ Core/Interface/Repository/IShiftRepository.cs
@@ -5,7 +5,7 @@ namespace Core.Interface.Repository
     public interface IShiftRepository
     {
         Task<List<ShiftEntity>> GetAsync(long userID, int year, int month);
-        Task<ShiftEntity?> GetAsync(long userID, DateTime date);
+        Task<ShiftEntity?> GetAsync(long userID, DateOnly date);
         Task<ShiftEntity?> GetByIDAsync(long shiftID);
         Task<long> CreateAsync(ShiftEntity model);
         Task UpdateAsync(ShiftEntity model);
--- WorkingHoursAPI/Mapper/ShiftMapper.cs
+++ WorkingHoursAPI/Mapper/ShiftMapper.cs
@@ -10,6 +10,9 @@ namespace WorkingHoursAPI.Mapper
     {
         public ShiftMapper() 
         {
+            CreateMap<ShiftPauseEntity, StartStopView>();
+            CreateMap<StartStopView, ShiftPauseEntity>();
+            CreateMap<DayTypeEntity, ShiftDayTypeView>();
             CreateMap<ShiftEntity, GetShiftView>()
                 .ForMember(dest => dest.Date, opt => opt.MapFrom(src => src.ShiftDate))
                 .ForMember(dest => dest.StartStop, opt => opt.MapFrom(src => new StartStopView
@@ -18,18 +21,10 @@ namespace WorkingHoursAPI.Mapper
                     Stop = src.Stop,
                 }))
                 .ForMember(dest => dest.Pauses, opt => opt.MapFrom(src => src.ShiftPauses));
-
-            CreateMap<ShiftPauseEntity, StartStopView>();
-            CreateMap<StartStopView, ShiftPauseEntity>()
-                .ForMember(dest => dest.Start, opt => opt.MapFrom(src => src.Start.ToUniversalTime()))
-                .ForMember(dest => dest.Stop, opt => opt.MapFrom(src => src.Stop.HasValue ? src.Stop.Value.ToUniversalTime() : (DateTime?)null));
-
-            CreateMap<DayTypeEntity, ShiftDayTypeView>();
-
             CreateMap<ShiftView, ShiftEntity>()
-                .ForMember(dest => dest.ShiftDate, opt => opt.MapFrom(src => src.Date.ToUniversalTime()))
-                .ForMember(dest => dest.Start, opt => opt.MapFrom(src => src.StartStop.Start.ToUniversalTime()))
-                .ForMember(dest => dest.Stop, opt => opt.MapFrom(src => src.StartStop.Stop.HasValue ? src.StartStop.Stop.Value.ToUniversalTime() : (DateTime?)null))
+                .ForMember(dest => dest.ShiftDate, opt => opt.MapFrom(src => src.Date))
+                .ForMember(dest => dest.Start, opt => opt.MapFrom(src => src.StartStop.Start))
+                .ForMember(dest => dest.Stop, opt => opt.MapFrom(src => src.StartStop.Stop))
                 .ForMember(dest => dest.ShiftPauses, opt => opt.MapFrom(src => src.Pauses));
         }
     }
